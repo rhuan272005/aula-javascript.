b352700b6e1fa6471391b8f011766a0d681efba0
Rhuan Augusto <134159474+rhuan272005@users.noreply.github.com>
2023-05-20 14:06:52 -0300

    Arquivos aula 1

---
+++ ola-mundo.js
@@ -0,0 +1,15 @@
+//alert("Este alerta esta vindo de outro arquivo")
+mensagem = "Esta mensagem é através de uma varivel" //string
+numero_inteiro = 23 //int
+console.log("uma mensagem no console");
+boolear = true 
+    array = ["Rhuan", 17, 1.71, true]
+    
+    nome = "Rhuan Augusto"
+    idade= 17
+    altura = 1.71
+    maior_idade = true
+    cidade = "Americana"
+    numero_casa = 1317
+    
+    meus_dados = []
